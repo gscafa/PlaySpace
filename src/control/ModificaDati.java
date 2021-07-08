c4f3e55907708b46b926602a4a9a783f8429ed0e
Utente <Utente@192.168.1.153>
2021-07-08 17:03:43 +0200

    modifica dati ha bisogno di prendere username password e email da database e inserire collegamento a index

--- src/control/ModificaDati.java
+++ src/control/ModificaDati.java
@@ -1,11 +1,19 @@
 package control;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.DriverManagerConnectionPool;
+import model.Utente;
 
 /**
  * Servlet implementation class ModificaDati
@@ -28,10 +36,57 @@ public class ModificaDati extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
+		String user = request.getParameter("username");
+		String passwordv = request.getParameter("passwordv");
+		String passwordn = request.getParameter("passwordn");
 		
+		boolean a=false;
+		boolean found = false;
+		HttpSession session = request.getSession();
+		session.setAttribute("logged", true);
+		try {
+		Connection con = DriverManagerConnectionPool.getConnection();
+		PreparedStatement st = con.prepareStatement("select * from utente where username = ? and password = ?;");
+		st.setString(1, user);
+		st.setString(2, passwordv);
+		
+		ResultSet rs = st.executeQuery();
+		
+		
+		
+		if(rs.next()) {
+			
+			found = true;
+
+			rs.updateObject(0, user);
+			if(passwordv == rs.getObject(3))
+				rs.updateObject(3, passwordn);
+		
+		}
+	}
+		
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		/*if(found) {
+			
+			
+			session.setAttribute("user", user);
+			if(user != null) 
+				session.setAttribute("logged", true);
+			if(a)
+				session.setAttribute("admin", true);
+			session.setAttribute("userFound", true);
+			request.getRequestDispatcher("index.jsp").forward(request,response);
+			}
+			else {
+				session.setAttribute("userFound", false);
+				request.getRequestDispatcher("login.jsp").forward(request,response);
+			}
+			
+			*/
 	
 	}
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
